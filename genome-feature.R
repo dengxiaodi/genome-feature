134c3bebbc6e648b5395811b3cc4c884d3cc8a61
dengxiaodi <dixiaodeng@gmail.com>
2016-06-28 15:30:23 +0800

    output content

--- genome-feature.R
+++ genome-feature.R
@@ -65,6 +65,7 @@ genes.seg.bins$hits[stats[, 1]] <- stats[, 2]
 
 total.hits <- sum(genes.seg.bins$hits)
 dist <- genes.seg.bins[, .(p = sum(hits) / total.hits), by = "bId"]
+dist$tissue <- tussue
 out.filename <- paste("gene.pos", tissue, rtype, "csv", sep = ".")
 write.csv(dist, out.filename, row.names = FALSE, quote = FALSE)
 
